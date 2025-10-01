974189b697baa216518e9fe92d17eab15ce7e028
Velmurugan.P <gunavel2580@gmail.com>
2025-10-01 08:41:58 +0530

    fix

--- php/config.php
+++ php/config.php
@@ -26,7 +26,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit(0);
 }
 ?>
